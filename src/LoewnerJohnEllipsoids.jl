51ce7703b0831126979893e00bc9a97ae30a1d05
Robin Deits <robin.deits@gmail.com>
2016-02-09 16:44:56 -0500

    implement inner and outer ellipsoids, plus a few tests

--- src/LoewnerJohnEllipsoids.jl
+++ src/LoewnerJohnEllipsoids.jl
@@ -1,5 +1,70 @@
 module LoewnerJohnEllipsoids
 
-# package code goes here
+using Convex
+export inner_ellipsoid,
+	   outer_ellipsoid,
+	   box
+
+type LinearConstraint{T<:Real}
+	# Represents the constraint a * x <= b
+	a::Vector{T}
+	b::T
+end
+
+type Ellipsoid{T}
+	C::Array{T, 2}
+	d::Array{T, 1}
+end
+
+Ellipsoid{T}(C::Array{T,2}, d::Array{T,2}) = Ellipsoid(C, vec(d))
+
+type Polyhedron{T}
+	constraints::Vector{LinearConstraint{T}}
+end
+
+function box{T}(lower::Vector{T}, upper::Vector{T})
+	@assert length(lower) == length(upper)
+	dimension = length(lower)
+	constraints = Array(LinearConstraint{T}, 2 * dimension)
+	for i = 1:dimension
+		a = zeros(T, dimension)
+		a[i] = 1
+		constraints[i] = LinearConstraint(a, upper[i])
+	end
+	for i = 1:dimension
+		a = zeros(T, dimension)
+		a[i] = -1
+		constraints[i + dimension] = LinearConstraint(a, -lower[i])
+	end
+	Polyhedron(constraints)
+end
+
+function inner_ellipsoid{T<:Real}(polyhedron::Polyhedron{T})
+	@assert length(polyhedron.constraints) > 2
+	dimension = length(polyhedron.constraints[1].a)
+	C = Variable(dimension, dimension)
+	d = Variable(dimension)
+	problem = maximize(logdet(C))
+	for constraint in polyhedron.constraints
+		problem.constraints += norm(C*constraint.a) <= constraint.b - dot(constraint.a, d)
+	end
+	solve!(problem)
+	return Ellipsoid(C.value, d.value)
+end
+
+function outer_ellipsoid{T}(points::Vector{T})
+	@assert length(points) > 1
+	dimension = length(points[1])
+	P = Variable(dimension, dimension)
+	q = Variable(dimension)
+	problem = maximize(logdet(P))
+	for point in points
+		problem.constraints += norm(P*point - q) <= 1
+	end
+	solve!(problem)
+	P, q = P.value, q.value
+	C = inv(P)
+	return Ellipsoid(C, C * q)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,30 @@ using LoewnerJohnEllipsoids
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+function test_unit_circle()
+	polyhedron = box([-1.,-1], [1., 1])
+	e = inner_ellipsoid(polyhedron)
+	@show e
+	@test isapprox(e.d, [0,0], atol=1e-4)
+	@test isapprox(e.C, [1 0; 0 1], atol=1e-4)
+end
+test_unit_circle()
+
+function test_shifted_scaled_box()
+	polyhedron = box([1,1], [2,2])
+	e = inner_ellipsoid(polyhedron)
+	@show e
+	@test isapprox(e.d, [1.5, 1.5], atol=1e-4)
+	@test isapprox(e.C, [0.5 0; 0 0.5], atol=1e-4)
+end
+test_shifted_scaled_box()
+
+function test_outer_unit_box()
+	points = Any[[-1,-1], [-1,1], [1,-1], [1,1]]
+	e = outer_ellipsoid(points)
+	@show e
+	@test isapprox(e.d, [0, 0], atol=1e-4)
+	@test isapprox(e.C, [sqrt(2) 0; 0 sqrt(2)], atol=1e-4)
+end
+test_outer_unit_box()
+
